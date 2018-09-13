87003940d4aa64f9247bb0868dd2b41d1b68475c
demichka <demichka@gmail.com>
2018-09-13 15:21:41 +0200

    week-lunch.html begins

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
-    var innerHeight = window.innerHeight;
-    $('.full-height').innerHeight(innerHeight);
-});
+// $(document).ready(function(){
+//     var innerHeight = window.innerHeight;
+//     $('.full-height').innerHeight(innerHeight);
+// });
