3498b8e0b4e37966123877b33e8848e438355dea
demichka <demichka@gmail.com>
2018-09-26 12:23:12 +0200

    added modal modified button Order added form to modal

--- www/js/main.js
+++ www/js/main.js
@@ -6,31 +6,53 @@ $(document).ready(function () {
     let switchEnglish = $('.switch-english');
     let carousel = $('.owl-carousel');
     let lang = 'sv';
-
-
-
     let d = new Date();
     let dayOfWeek = d.getDay();
     let dayName = '';
-    let weekDays = [
-        'Måndag',
-        'Tisdag',
-        'Onsdag',
-        'Torsdag',
-        'Fredag'
-    ];
+    let weekDays = [{
 
-    function setDayName() {
+            name: 'sv',
+            days: [
+                'Måndag',
+                'Tisdag',
+                'Onsdag',
+                'Torsdag',
+                'Fredag',
+                'Lördag',
+                'Söndag'
+            ]
+        },
+        {
+            name: 'en',
+            days: [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday'
+            ]
+        }
+    ]
+
+    function setDayName(langDay) {
+        if (lang === 'sv') {
+            langDay = 0;
+        } else {
+            langDay = 1;
+        }
+
         if (dayOfWeek === 1) {
-            dayName = weekDays[0];
+            dayName = weekDays[langDay].days[0];
         } else if (dayOfWeek === 2) {
-            dayName = weekDays[1];
+            dayName = weekDays[langDay].days[1];
         } else if (dayOfWeek === 3) {
-            dayName = weekDays[2];
+            dayName = weekDays[langDay].days[2];
         } else if (dayOfWeek === 4) {
-            dayName = weekDays[3];
+            dayName = weekDays[langDay].days[3];
         } else if (dayOfWeek === 5) {
-            dayName = weekDays[4];
+            dayName = weekDays[langDay].days[4];
         }
         $('.day-name').append(dayName);
     }
@@ -48,7 +70,6 @@ $(document).ready(function () {
         })
     }
 
-
     function loadWeekLunch(weekLunch) {
         showMenu(lang);
         runOwlCarousel();
@@ -59,11 +80,15 @@ $(document).ready(function () {
             $(this).siblings().removeClass('on').addClass('off');
             $('.lunch-list').empty();
             $('.week-lunch-img').empty();
+            $('.day-name').empty();
+            $('.text-place').empty();
+            $('.heading').empty();
             carousel.trigger('destroy.owl.carousel');
             checkingLanguage();
             showMenu();
             runOwlCarousel();
-            console.log(lang);
+            setDayName();
+            addOrderBtn();
         });
 
 
@@ -80,14 +105,12 @@ $(document).ready(function () {
 
         function showMenu(language) {
             language = lang;
-            console.log(language);
             let ul = $('<ul/>');
             let i = dayOfWeek;
             let currentDay = weekLunch[i];
 
 
             for (item in currentDay.lang[language]) {
-                console.log(currentDay.lang[language][item].name);
                 let meal = currentDay.lang[language][item];
                 let title = meal.name;
                 let mealDesc = meal.desc;
@@ -105,23 +128,39 @@ $(document).ready(function () {
             let everyDay = weekLunch[0];
             let pSmall = $('<p/>');
             for (let el in everyDay.lang[language]) {
-                let everyDayMeal = everyDay.lang[language][el];
-                pSmall.append('<small>' + everyDayMeal.name + '</small>');
+                let everyDayInfo = everyDay.lang[language][el];
+                pSmall.append('<small>' + everyDayInfo.name + '</small>');
+                $('.text-place').append(everyDayInfo.text);
+                $('.heading').append(everyDayInfo.title);
             }
             runOwlCarousel();
 
-            let btn = $('<button/>').attr('type', 'button').addClass('btn').html('Order');
+
 
             $('.lunch-list').append(ul);
             $('.lunch-list').append(pSmall);
-            $('.lunch-list').append(btn);
+
         }
 
 
 
-
-
-
     }
+
+    function addOrderBtn() {
+        let btn = $('<button/>').attr('type', 'button').addClass('order-btn btn').html('Order');
+        $('.lunch-list').append(btn);
+    }    
+
+    // addOrderBtn();
+    let orderBtn = $('.order-btn');
+    orderBtn.click(function () {
+        $('.modal').fadeIn();
+        $('.fade').fadeIn();
+    });
+
+    $('.close').click(function () {
+        $(this).parents('.modal').fadeOut();
+        $('.fade').fadeOut();
+    });
 
 });
