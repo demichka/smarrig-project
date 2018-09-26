78853e61ddd6f1a796566b01c6465b22df302432
demichka <demichka@gmail.com>
2018-09-26 23:03:49 +0200

    added validateForm

--- www/js/main.js
+++ www/js/main.js
@@ -149,46 +149,49 @@ $(document).ready(function () {
             $('.lunch-list').append(ul);
             $('.lunch-list').append(pSmall);
             $('.lunch-list').append(btn);
-            console.log('a', new Date());
 
         }
     }
 
-
-    let orderBtn = $('.order-btn');
-    console.log('b', new Date());
-    orderBtn.click(function () {
-        $('.modal').fadeIn();
-        $('.fade').fadeIn();
-    });
-
-
-
+    
+    $('#get-order-btn').click(getOrder);
 
     function getOrder() {
         let summary = {};
         summary.name = $('#namef').val();
         summary.email = $('#emailf').val();
         summary.meal = $('input:checked').val();
-        let summarizeP = $('<p/>');
-        summarizeP.append(
-            'Hej, ' + summary.name + '!' +
-            '<br/>' + 'We send your order to ' + summary.email + '.' +
-            '<br/>' + 'You ordered ' + summary.meal + ',' +
-            '<br/>' + 'Pick it up and enjoy your lunch!');
-        $('#form-modal').fadeOut();
-        let modalSum = $('<div class="modal" id="modal-answer"/>').append($('<div class="container"/>').append($('<div class="modal-inner"/>').append($('<span class="close"/>').append($('<i class="fas fa-times"/>'))).append($('<h3/>'))));
-        $('body').append(modalSum);
-        $('#modal-answer').append(summarizeP);
-        $('.close').click(function () {
-            $(this).parents('.modal').fadeOut();
-            $('.fade').fadeOut();
-            $('form')[0].reset();
-            $('#modal-answer').remove();
-        });
+        if(validateForm(summary)){
+            showOrder(summary);
+        }
+        else {
+            alert('0');
+        }
     }
 
-    $('#get-order-btn').click(getOrder);
+    function validateEmail(orderEmail) {
+        let email = orderEmail.email;
+        var result = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return result.test(String(email).toLowerCase());
+    }
+
+
+
+    function validateForm(orderData) {
+        let validName = orderData.name.length >= 2;
+        let validEmail = validateEmail(orderData);
+        let validMeal = !!orderData.meal;
+        console.log(validEmail);
+        console.log(validName);
+        const result = 
+            validName &&
+            validEmail &&
+            validMeal;
+        console.log(result);
+        return result;
+    }
+
+    
 
 
     $('.close').click(function () {
@@ -208,4 +211,24 @@ $(document).ready(function () {
         $('.fade').fadeOut();
     });
 
+
+
+    function showOrder(orderSummary) {
+        let summarizeP = $('<p/>');
+        summarizeP.append(
+            'Hej, ' + orderSummary.name + '!' +
+            '<br/>' + 'We send your order to ' + orderSummary.email + '.' +
+            '<br/>' + 'You ordered ' + orderSummary.meal + ',' +
+            '<br/>' + 'Pick it up and enjoy your lunch!');
+        $('#form-modal').fadeOut();
+        let modalSum = $('<div class="modal" id="modal-answer"/>').append($('<div class="container"/>').append($('<div class="modal-inner"/>').append($('<span class="close"/>').append($('<i class="fas fa-times"/>'))).append($('<h3/>'))));
+        $('body').append(modalSum);
+        $('#modal-answer').append(summarizeP);
+        $('.close').click(function () {
+            $(this).parents('.modal').fadeOut();
+            $('.fade').fadeOut();
+            $('form')[0].reset();
+            $('#modal-answer').remove();
+        });
+    }
 });
