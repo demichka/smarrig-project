9494e2ce028a3cfc5f9f8744c48f81873626df29
demichka <demichka@gmail.com>
2018-09-27 16:01:55 +0200

    fixed order-form fixed answer-message added weekend-message fixed grid-float fixed grid-flex release

--- www/js/main.js
+++ www/js/main.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
     let d = new Date();
     let dayOfWeek = d.getDay();
     let dayName = '';
+    const btnShowWholeWeekMenu = $('.show-whole-week-menu');
+    const wholeMenuHeading = $('#whole-week-menu h3');
     let weekDays = [{
 
             name: 'sv',
@@ -53,6 +55,10 @@ $(document).ready(function () {
             dayName = weekDays[langDay].days[3];
         } else if (dayOfWeek === 5) {
             dayName = weekDays[langDay].days[4];
+        } else if (dayOfWeek === 6) {
+            dayName = weekDays[langDay].days[5];
+        } else if (dayOfWeek === 0) {
+            dayName = weekDays[langDay].days[6];
         }
         $('.day-name').append(dayName);
     }
@@ -71,8 +77,9 @@ $(document).ready(function () {
     }
 
     function loadWeekLunch(weekLunch) {
-        showMenu(lang);
-        runOwlCarousel();
+        checkWeekend();
+        showWholeWeekMenu(lang);
+        changeLangShowWholeMenu(lang);
         setDayName();
 
         $(switchLanguageButton).click(function () {
@@ -84,12 +91,37 @@ $(document).ready(function () {
             $('.text-place').empty();
             $('.heading').empty();
             $('.meal-list-to-order').empty();
+            $('.whole-week-menu-list').empty();
+            $('.weekend-message').remove();
             carousel.trigger('destroy.owl.carousel');
             checkingLanguage();
-            showMenu();
             runOwlCarousel();
             setDayName();
+            showWholeWeekMenu();
+            changeLangShowWholeMenu();
+            checkWeekend();
         });
+
+
+        function checkWeekend(l) {
+            l = lang;
+            carousel.trigger('destroy.owl.carousel');
+            if (dayOfWeek !== 6 && dayOfWeek !== 0) {
+                showMenu(lang);
+
+            } else {
+                let weekendMessage = $('<p class="weekend-message"/p>');
+                if (l === 'sv') {
+                    weekendMessage.append('Det finns ingen veckan meny på helgen!<br/> Återkomma på måndag!<br/>' +
+                        'Du kan trycka på knappen ovan för att öppna Hela Veckans lunch meny.');
+                }
+                if (l === 'en') {
+                    weekendMessage.append('We don\'t serve week\'s lunch on weeekend!' + '<br/>' + 'Welcome back on Monday!' + '<br/>' + 'Click the button above to open Whole week\'s lunch menu.');
+                }
+
+                $('.lunch-menu').append(weekendMessage);
+            }
+        }
 
 
 
@@ -109,8 +141,7 @@ $(document).ready(function () {
             let i = dayOfWeek;
             let currentDay = weekLunch[i];
             let mealListToOrder = $('.meal-list-to-order');
-
-            for (item in currentDay.lang[language]) {
+            for (let item = 1; item < currentDay.lang[language].length; item++) {
                 let meal = currentDay.lang[language][item];
                 let title = meal.name;
                 let mealDesc = meal.desc;
@@ -124,6 +155,7 @@ $(document).ready(function () {
                 mealImage.find('figcaption').append(mealDesc);
                 mealListToOrder
                     .append($('<div class="form-group"/>')
+                        .append($('<input class="hidden-desc" type="hidden" />').attr('value', mealDesc))
                         .append($('<input type="radio" name="select-meal"/>').attr('id', title).attr('value', title))
                         .append($('<label/>')
                             .append(title).attr('for', title))
@@ -141,19 +173,71 @@ $(document).ready(function () {
 
             let btn = $('<button class="btn order-btn">Order</button>');
             btn.click(function () {
-                $('.modal').fadeIn();
+                $('#form-modal').fadeIn();
                 $('.fade').fadeIn();
             });
+
+
+
 
             runOwlCarousel();
             $('.lunch-list').append(ul);
             $('.lunch-list').append(pSmall);
             $('.lunch-list').append(btn);
 
+
         }
+
+        function showWholeWeekMenu(l) {
+            l = lang;
+            let dl = $('<dl/>');
+            let wholeWeek = weekLunch;
+
+            for (item in wholeWeek) {
+                let day = wholeWeek[item].lang[l];
+                let dayName = day[0].nameOfWeek;
+                let dt = $('<dt/>');
+                dt.append(dayName);
+                dl.append(dt);
+                for (let m = 0; m < day.length; m++) {
+                    let meal = day[m];
+                    let dd = $('<dd/>');
+                    dd.append(meal.name);
+                    dd.insertAfter(dt);
+                }
+
+            }
+
+            $('.whole-week-menu-list').append(dl);
+
+        }
+
+
+        btnShowWholeWeekMenu.click(function () {
+            $('#whole-week-menu').fadeIn();
+            $('.fade').fadeIn();
+        });
+
+        function changeLangShowWholeMenu(l) {
+            l = lang;
+            btnShowWholeWeekMenu.empty();
+            wholeMenuHeading.empty();
+
+            if (l === 'sv') {
+                btnShowWholeWeekMenu.append('Visa veckans meny');
+                wholeMenuHeading.append('Veckans lunch meny');
+            } else if (l === 'en') {
+                btnShowWholeWeekMenu.append('Open whole week\'s menu');
+                wholeMenuHeading.append('Whole week\'s menu');
+            }
+        }
+
+
+
+
     }
 
-    
+
     $('#get-order-btn').click(getOrder);
 
     function getOrder() {
@@ -161,11 +245,9 @@ $(document).ready(function () {
         summary.name = $('#namef').val();
         summary.email = $('#emailf').val();
         summary.meal = $('input:checked').val();
-        if(validateForm(summary)){
+        summary.desc = $('input:checked').siblings('.hidden-desc').val();
+        if (validateForm(summary)) {
             showOrder(summary);
-        }
-        else {
-            alert('0');
         }
     }
 
@@ -181,33 +263,65 @@ $(document).ready(function () {
         let validName = orderData.name.length >= 2;
         let validEmail = validateEmail(orderData);
         let validMeal = !!orderData.meal;
-        console.log(validEmail);
-        console.log(validName);
-        const result = 
-            validName &&
+        let desc = !!orderData.desc;
+        let errorName = $('<span class="invalid-input error-name">Enter your name please!</span>');
+        let errorEmail = $('<span class="invalid-input error-email">Enter valid email please!</span>');
+        let errorMeal = $('<span class="invalid-input error-meal">Choose at least one meal please!</span>');
+
+
+
+        $('.invalid-input').remove();
+
+        if (validName &&
             validEmail &&
-            validMeal;
-        console.log(result);
-        return result;
+            validMeal) {
+            return true;
+        } else {
+
+            if (!validName) {
+
+                $(errorName).insertAfter($('#namef'));
+            }
+            if (!validEmail) {
+                $(errorEmail).insertAfter($('#emailf'));
+            }
+            if (!validMeal) {
+                $(errorMeal).insertAfter($('.meal-list-to-order'));
+            }
+            if (validName) {
+                $(this).next('.invalid-input').remove();
+            }
+            if (validEmail) {
+                $('.error-email').remove();
+            }
+            if (validMeal) {
+                $('.error-meal').remove();
+            }
+            return false;
+        }
     }
 
-    
+
+
 
 
     $('.close').click(function () {
         $(this).parents('.modal').fadeOut();
         $('.fade').fadeOut();
+        $('.invalid-input').remove();
         $('form')[0].reset();
     });
 
     $('.fade').click(function () {
         $(this).fadeOut();
         $('.modal').fadeOut();
+        $('.invalid-input').remove();
         $('form')[0].reset();
     });
 
     $('.btn:reset').click(function () {
         $(this).parents('.modal').fadeOut();
+        $('.invalid-input').remove();
         $('.fade').fadeOut();
     });
 
@@ -216,12 +330,13 @@ $(document).ready(function () {
     function showOrder(orderSummary) {
         let summarizeP = $('<p/>');
         summarizeP.append(
-            'Hej, ' + orderSummary.name + '!' +
-            '<br/>' + 'We send your order to ' + orderSummary.email + '.' +
-            '<br/>' + 'You ordered ' + orderSummary.meal + ',' +
+            'Hej, ' + '<strong>' + orderSummary.name + '</strong>' + '!' +
+            '<br/>' + 'We send your order to ' + '<strong>' + orderSummary.email + '</strong>' + '.' +
+            '<br/>' + 'You ordered ' + '<strong>' + orderSummary.meal + '</strong>' + ',' +
+            '<br/>' + 'By the way, you will eat ' + '<strong>' + orderSummary.desc + '</strong>' + ',' +
             '<br/>' + 'Pick it up and enjoy your lunch!');
         $('#form-modal').fadeOut();
-        let modalSum = $('<div class="modal" id="modal-answer"/>').append($('<div class="container"/>').append($('<div class="modal-inner"/>').append($('<span class="close"/>').append($('<i class="fas fa-times"/>'))).append($('<h3/>'))));
+        let modalSum = $('<div class="modal" id="modal-answer"><div class="container"><div class="modal-inner"><span class="close"><i class="fas fa-times"></i></span><h3>Yeepi! You\'ve just ordered!</h3></div></div></div>');
         $('body').append(modalSum);
         $('#modal-answer').append(summarizeP);
         $('.close').click(function () {
